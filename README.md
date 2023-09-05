ab4536b52d1b1887df538f9bbf4492c242383892
Theo <ekunnusitheophilus@gmail.com>
2023-09-05 14:40:20 +0100

    redis fix

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # alx-files_manager
+
+# Resources
+- [NodeJS getting started](https://intranet.alxswe.com/rltoken/8jNm2s_LfVKMqR3vHLn_uw)
+- [Process API Doc](https://intranet.alxswe.com/rltoken/uYPplj2cPK8pcP0LtV6RuA)
+- [express getting started](https://intranet.alxswe.com/rltoken/SujfeWKCWmUMomfETjETEg)
+- [Mocha documentation](https://intranet.alxswe.com/rltoken/FzEwplmoZiyGvkgKllZNJw)
+- [Nodemon documentation](https://intranet.alxswe.com/rltoken/pdNNTX0OLugbhxvP3sLgOw)
+- [MongoDB](https://intranet.alxswe.com/rltoken/g1x7y_3GskzVAJBTXcSjmA)
+- [Bull](https://intranet.alxswe.com/rltoken/NkHBpGrxnd0sK_fDPMbihg)
+- [Image thumbnail](https://intranet.alxswe.com/rltoken/KX6cck2nyLpQOTDMLcwxLg)
+- [mime-types](https://intranet.alxswe.com/rltoken/j9B0Kc-4HDKLUe88ShbOjQ)
+- [Redis](https://intranet.alxswe.com/rltoken/nqwKRszO8Tkj_ZWW1EFwGw)
---
+++ utils/redis.js
@@ -0,0 +1,54 @@
+/* eslint-disable class-methods-use-this */
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.log(`Redis not connected to server: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return true;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data === 1);
+        }
+      });
+    });
+  }
+}
+
+export const redisClient = new RedisClient();
